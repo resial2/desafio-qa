df0fe118c98855dea476c96a48cfb21a0a7d02c8
João Rener <joao.rener42@gmail.com>
2020-05-21 20:42:00 -0300

    Adicionado Runner WEb

--- src/test/java/runner/RunTestWEB.java
+++ src/test/java/runner/RunTestWEB.java
@@ -12,13 +12,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/features",
-        tags = {"@API"},
+        tags = {"@LOGIN"},
         strict = true,
-        glue = {"stepdef", "pretest"},
+        glue = {"stepdef/web"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 
-public class RunTest {
+public class RunTestWEB {
 
     @BeforeClass
     public static void setup(){
