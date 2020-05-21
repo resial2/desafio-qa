a474c34d7df28c7da9388264201d2ca98bc4d1f0
João Rener <joao.rener42@gmail.com>
2020-05-21 12:10:00 -0300

    Adicionada lógica da automação

--- src/test/java/stepdef/web/BaseSteps.java
+++ src/test/java/stepdef/web/BaseSteps.java
@@ -5,9 +5,13 @@ import io.cucumber.java.After;
 import io.cucumber.java.pt.*;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.junit.Assert;
 import qa.desafio.Utils;
 import qa.desafio.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import qa.desafio.pageobjects.automationpractice.Home;
+import qa.desafio.pageobjects.automationpractice.Login;
+import qa.desafio.pageobjects.automationpractice.UserMainPage;
 
 
 public class BaseSteps {
@@ -28,28 +32,52 @@ public class BaseSteps {
         driver.quit();
     }
 
+    private Home home;
+    private Login login;
+    private UserMainPage userMainPage;
+
     @Dada("abertura do site na página principal")
     public void abertura_do_site_na_página_principal() {
-
+        home = new Home(driver);
+        home.acessarPaginaInicial();
     }
 
     @Quando("clicar no botão {string}")
     public void clicarNoBotão(String button) {
+
+        switch(button.toUpperCase()){
+            case "SIGN IN":
+                login = home.clicarBotaoSignIn();
+                break;
+            default:
+                Assert.fail("Botão não configurado.");
+        }
+
     }
 
     @E("preencher os campos de e-mail e senha com as informações do arquivo de {string}")
-    public void preencherOsCamposDeEMailESenhaComAsInformaçõesDoArquivoDe(String file) throws ConfigurationException {
+    public void preencherOsCamposDeEMailESenhaComAsInformaçõesDoArquivoDe(String file) {
         PropertiesConfiguration props = Utils.getProperties(propertiesConfig.getString(file));
-
+        login.preencherEmail(props.getString("valid.email"))
+                .preencherSenha(props.getString("valid.password"));
     }
 
     @E("clicar no botão {string} para entrar")
     public void clicarNoBotãoParaEntrar(String button) {
+        switch (button.toUpperCase()){
+            case "SIGN IN":
+                userMainPage = login.clicarBotaoLogin();
+                break;
+            default:
+                Assert.fail("Botão não configurado.");
+        }
     }
 
     @E("preencher os campos de e-mail e senha com as informações inválidas do arquivo de {string}")
     public void preencherOsCamposDeEMailESenhaComAsInformaçõesInválidasDoArquivoDe(String file) {
         PropertiesConfiguration props = Utils.getProperties(propertiesConfig.getString(file));
+        login.preencherEmail(props.getString("invalid.email"))
+                .preencherSenha(props.getString("invalid.password"));
     }
 
     @E("não realizar o preenchimento dos campos de e-mail e senha")
@@ -58,10 +86,14 @@ public class BaseSteps {
 
     @Entao("deverá ser exibida a pagina principal do usuário")
     public void deveráSerExibidaAPaginaPrincipalDoUsuário() {
+        Assert.assertTrue("Login não realizado", userMainPage.verificarLoginComSucesso());
+        System.out.println("Login realizado com êxito.");
     }
 
     @Entao("deverá ser exibida a mensagem {string}")
-    public void deveráSerExibidaAMensagem(String expectedMessage) {
+    public void deveráSerExibidaAMensagem(String mensagemEsperada) {
+        String textoApresentado = login.pegarTextoAlerta();
+        Assert.assertEquals("Texto esperado e Texto apresentado estão divergentes. [ Apresentado: " + textoApresentado +" ] [ Esperado: " + mensagemEsperada +" }", mensagemEsperada, textoApresentado);
     }
 
 
