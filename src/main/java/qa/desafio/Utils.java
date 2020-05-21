c8f2aea8c3598b2755111de451ad6c01719f9faa
João Rener <joao.rener42@gmail.com>
2020-05-21 17:20:48 -0300

    Adicionados métodos para geração de massa!

--- src/main/java/qa/desafio/Utils.java
+++ src/main/java/qa/desafio/Utils.java
@@ -2,6 +2,9 @@ package qa.desafio;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.junit.Assert;
+
+import java.util.Random;
 
 
 public class Utils {
@@ -15,4 +18,24 @@ public class Utils {
         }
     }
 
+    public static int gerarNumeroRandom(int limiteInicial, int limiteFinal){
+        if(limiteInicial > limiteFinal){
+            Assert.fail("O limite inicial não pode ser superior ao limite final.");
+        }
+
+        Random rand = new Random();
+        return rand.nextInt((limiteFinal - limiteInicial) + 1) + limiteInicial;
+    }
+
+    public static String gerarLetrasAleatorias(int tamanhoFinal){
+        final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        final int N = alphabet.length();
+        Random rd = new Random();
+        StringBuilder sb = new StringBuilder(tamanhoFinal);
+        for (int i = 0; i < tamanhoFinal; i++) {
+            sb.append(alphabet.charAt(rd.nextInt(N)));
+        }
+
+        return sb.toString();
+    }
 }
