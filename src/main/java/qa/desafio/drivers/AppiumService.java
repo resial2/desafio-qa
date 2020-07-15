e500e05a7076f2f02e625f083d72ae3ca63b099f
João Rener <joao.rener42@gmail.com>
2020-07-15 16:15:45 -0300

    Atualizado start do Appium

--- src/main/java/qa/desafio/drivers/AppiumService.java
+++ src/main/java/qa/desafio/drivers/AppiumService.java
@@ -2,6 +2,7 @@ package qa.desafio.drivers;
 
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
@@ -24,12 +25,44 @@ public class AppiumService {
         }
     }
 
+    public static void startAppiumServiceDesktop() {
+        AppiumServiceBuilder asb;
+        DesiredCapabilities cap;
+
+        cap = new DesiredCapabilities();
+        cap.setCapability("noReset", "false");
+        asb = new AppiumServiceBuilder();
+        asb.withIPAddress("0.0.0.0");
+        asb.usingPort(4723);
+        asb.withCapabilities(cap);
+        asb.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        asb.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+        service = AppiumDriverLocalService.buildService(asb);
+        service.start();
+    }
+
+    public static void startAppiumServiceDesktop(String host, int port) {
+        AppiumServiceBuilder asb;
+        DesiredCapabilities cap;
+
+        cap = new DesiredCapabilities();
+        cap.setCapability("noReset", "false");
+        asb = new AppiumServiceBuilder();
+        asb.withIPAddress(host);
+        asb.usingPort(port);
+        asb.withCapabilities(cap);
+        asb.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        asb.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+        service = AppiumDriverLocalService.buildService(asb);
+        service.start();
+    }
+
     public static void stopAppiumService() throws IOException {
         service.stop();
         Runtime.getRuntime().exec("taskkill /F /IM node.exe");
     }
 
-    public static URL getServiceUrl(){
+    public static URL getServiceUrl() {
         return service.getUrl();
     }
 
